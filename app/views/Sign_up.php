6e7245f0254bd7eed8f72ccd167626370003159f
Amanuel <amanuelbehailu557@gmail.com>
2026-05-19 17:11:22 +0300

    form for registration being worked on

--- app/views/Sign_up.php
+++ app/views/Sign_up.php
@@ -12,12 +12,19 @@
         <input type = "text" placeholder = "Abebe Bekele" name = "Fullname"/>
         <label>Email</label>
         <input type = "Email" placeholder = "AbebeBekele@gmail.com" name = "email"/>
-        <label>Year</label>
-        <input type = "text" placeholder = "2016" name = "year" inputmode="numeric" maxlength="4"/>
         <label>Department</label>
         <select name="Department" required>
-        <option value = "" >Select Department</option>
-        <option value = "">Computer Science</option>
+        <option value = "" >None</option>
+        <option value = "ComputerScience">Computer Science</option>
+        <option value = "Marketing_and_Management">Marketing and Management</option>
+        <option value = "Accounting">Accounting</option>
+        <option value = "Economics">Economics</option>
+        </select>
+        <label>Year</label>
+        <input type = "text" placeholder = "2016" name = "year" inputmode="numeric" maxlength="4"/>
+        <label>Semester</label>
+        <select name = "Semester" required>
+        
         </select>
         </form>
         </div>
