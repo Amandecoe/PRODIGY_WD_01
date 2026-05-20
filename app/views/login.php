ef739909db91f6b4498b57f0b036a147de36d3b5
Amanuel <amanuelbehailu557@gmail.com>
2026-05-20 14:21:14 +0300

    login page html

--- app/views/login.php
+++ app/views/login.php
@@ -7,6 +7,15 @@
             <button>Student</button>
             <button>Lecturer</button>
         </div>
+        <div id = "Student">
+            <form action = "../controllers/authenticationcontroller.php" method = "post">
+                <label>Email</label>
+                <input type = "email" name = "email" placeholder = "AbebeBekele@gmail.com" required>
+                <label>Password</label>
+                <input type="password" name = "password" placeholder = "user123" required>
+                <button name = "Login"> Sign Up</button>
+            </form>
+        </div>
         </div>
     </body>
 </html>
