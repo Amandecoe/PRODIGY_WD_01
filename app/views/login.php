e8717b8be07657b934cbb6660073da9c6a5961fe
Amanuel <amanuelbehailu557@gmail.com>
2026-05-22 18:17:25 +0300

    adding svgs

--- app/views/login.php
+++ app/views/login.php
@@ -5,6 +5,7 @@
     </head>
     <body>
         <div class="form-container">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M480-120 200-272v-240L40-600l440-240 440 240v320h-80v-276l-80 44v240L480-120Zm0-332 274-148-274-148-274 148 274 148Zm0 241 200-108v-151L480-360 280-470v151l200 108Zm0-241Zm0 90Zm0 0Z"/></svg>
             <h2>Welcome Back</h2>
             <p>Sign in into you account</p>
         <div class = "choice-button">
@@ -20,16 +21,18 @@
                 <input type="hidden" name="role" value="student">
                 <button id="final" type="submit" name = "Login"> Sign In</button>
             </form>
+            <p><a href="./Sign_up.php">Create an Account?</a></p>
         </div>
         <div id = "Lecturer">
             <form action = "../controllers/LecturerController.php" method = "post">
                 <label>Email</label>
-                <input type = "email" name = "email" placeholder = "AbebeBekele@gmail.com" required>
+                <input type = "email" name = "email" placeholder = "BirukAbebe@gmail.com" required>
                 <label>Password</label>
                 <input type="password" name = "password" placeholder = "abebe123" required>
                 <input type="hidden" name="role" value="lecturer">
                 <button id="final" type="submit" name = "Login">Sign In</button>
             </form>
+            <p><a href="./Sign_up.php">Create an Account?</a></p>
         </div>
         </div>
     </body>
