11aa5c2d4638ad743f23396bb39886492adb2160
Amanuel <amanuelbehailu557@gmail.com>
2026-05-22 18:21:09 +0300

    enough css for now on auth

--- app/views/login.php
+++ app/views/login.php
@@ -5,9 +5,9 @@
     </head>
     <body>
         <div class="form-container">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M480-120 200-272v-240L40-600l440-240 440 240v320h-80v-276l-80 44v240L480-120Zm0-332 274-148-274-148-274 148 274 148Zm0 241 200-108v-151L480-360 280-470v151l200 108Zm0-241Zm0 90Zm0 0Z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" height="35px" viewBox="0 -960 960 960" width="35px" fill="#000000"><path d="M480-120 200-272v-240L40-600l440-240 440 240v320h-80v-276l-80 44v240L480-120Zm0-332 274-148-274-148-274 148 274 148Zm0 241 200-108v-151L480-360 280-470v151l200 108Zm0-241Zm0 90Zm0 0Z"/></svg>
             <h2>Welcome Back</h2>
-            <p>Sign in into you account</p>
+            <p>Sign in into your account</p>
         <div class = "choice-button">
             <button onclick="choice()" id = "Student-choice">Student</button>
             <button onclick ="choice()" id="Lecturer-choice">Lecturer</button>
@@ -21,7 +21,7 @@
                 <input type="hidden" name="role" value="student">
                 <button id="final" type="submit" name = "Login"> Sign In</button>
             </form>
-            <p><a href="./Sign_up.php">Create an Account?</a></p>
+            <p id="link"><a href="./Sign_up.php">Create an Account?</a></p>
         </div>
         <div id = "Lecturer">
             <form action = "../controllers/LecturerController.php" method = "post">
@@ -32,7 +32,7 @@
                 <input type="hidden" name="role" value="lecturer">
                 <button id="final" type="submit" name = "Login">Sign In</button>
             </form>
-            <p><a href="./Sign_up.php">Create an Account?</a></p>
+            <p id="link"><a href="./Sign_up.php">Create an Account?</a></p>
         </div>
         </div>
     </body>
