82f4d30a5478bdc8bd46c80b9d323e291c0a73c5
Amanuel <amanuelbehailu557@gmail.com>
2026-05-22 20:26:40 +0300

    auth page css done

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,3 +1,4 @@
+window.addEventListener("DOMContentLoaded", () => {
 let student = document.getElementById("Student");
 let lecturer = document.getElementById("Lecturer");
 let studentchoicebutton = document.getElementById("Student-choice");
@@ -6,16 +7,24 @@ let lecturerchoicebutton = document.getElementById("Lecturer-choice");
 student.style.display = "inline"; //default when the pages load
 lecturer.style.display = "none";
 
+studentchoicebutton.classList.add("active");
+lecturerchoicebutton.classList.remove("active");
+
 function choice(){
     studentchoicebutton.addEventListener("click", () => {
         student.style.display = "inline";
         lecturer.style.display = "none";
+
+        studentchoicebutton.classList.add("active");
+        lecturerchoicebutton.classList.remove("active");
     });
 
     lecturerchoicebutton.addEventListener("click", () => {
         lecturer.style.display = "inline";
         student.style.display = "none";
+        lecturerchoicebutton.classList.add("active");
+            studentchoicebutton.classList.remove("active");
     })};
-
-    
+choice();
+});
 
