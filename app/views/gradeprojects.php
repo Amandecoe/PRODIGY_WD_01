2ed6a0c78acb7cc2d59dda719e4c65301aadec4f
Amanuel <amanuelbehailu557@gmail.com>
2026-05-28 15:02:49 +0300

    form for grading projects

--- app/views/gradeprojects.php
+++ app/views/gradeprojects.php
@@ -1,5 +1,11 @@
 <html>
     <body>
-        
+        <form>
+            <label>Lecturer Name</label>
+            <input type = "text" name="lecturer_name" required>
+            <label>Grade</label>
+            <input type = "text" name="grade" placeholder="A+, A, A- ...">
+            <button type="submit" name="graded">Grade</button>
+        </form>
     </body>
 </html>
