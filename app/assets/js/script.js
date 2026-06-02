e66c4d2bc7fff0ea232591ff0bfc8a54b42a088a
Amanuel <amanuelbehailu557@gmail.com>
2026-06-02 09:12:18 +0300

    fix

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -4,6 +4,7 @@ let lecturer = document.getElementById("Lecturer");
 let studentchoicebutton = document.getElementById("Student-choice");
 let lecturerchoicebutton = document.getElementById("Lecturer-choice");
 let Dueproject = document.getElementById("Due");
+let Submitted = document.getElementById("Submitted");
 
 student.style.display = "inline"; //default when the pages load
 lecturer.style.display = "none";
