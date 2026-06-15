2e86db6edb9b30b6e9002b90b3fe1bf183304f2e
Amanuel <amanuelbehailu557@gmail.com>
2026-06-15 04:23:23 +0300

    styling the form

--- app/views/submit_project.php
+++ app/views/submit_project.php
@@ -6,33 +6,19 @@
     <link rel="stylesheet" href="../assets/css/form.css">
     <title>Upload Project</title>
 </head>
-
 <body>
-
     <div class="form-container">
-
-        <a href="../controllers/DashboardController.php"
-           class="close-btn">
+        <a href="../controllers/DashboardController.php" class="close-btn">
             &times;
         </a>
-
         <h2>Upload Project</h2>
-
-        <form action="../controllers/Projectcontroller.php"
-              method="post"
-              enctype="multipart/form-data">
+        <form action="../controllers/Projectcontroller.php"method="post" enctype="multipart/form-data">
 
             <label for="title">Project Title</label>
-            <input id="title"
-                   type="text"
-                   name="project-title"
-                   required>
+            <input id="title"type="text" name="project-title" required>
 
             <label for="description">Project Description</label>
-            <textarea id="description"
-                      name="project-description"
-                      rows="4"
-                      required></textarea>
+            <textarea id="description" name="project-description" rows="4" required></textarea>
 
             <label for="file">File</label>
             <input id="file" type="file" name="project-file" required>
@@ -41,10 +27,7 @@
                     class="submit-btn">
                 Upload
             </button>
-
         </form>
-
     </div>
-
 </body>
 </html>
